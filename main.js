acfd2f11658d988ffcaf9f895999169d67545eea
NiketXS <nikotomczak@gmail.com>
2024-10-21 17:15:38 +0200

    Dodano Spritesheet

--- main.js
+++ main.js
@@ -1,63 +1,98 @@
-window.addEventListener("load",()=>{
+
+window.addEventListener("load",()=> {
     const canvas = document.getElementById("myCanvas")
     const ctx = canvas.getContext("2d")
     canvas.width = 800
     canvas.height = 720
-
+    const BASE_SPRITE_X_OFFSET = 10
+    const BASE_SPRITE_Y_OFFSET = 30
     class Player {
-        constructor(gameHeight,gameWidth){
+        constructor(gameHeight, gameWidth) { 
             this.gameHeight = gameHeight
             this.gameWidth = gameWidth
-            this.width = 200
-            this.height = 200
-            this.x = 100
-            this.y = this.gameHeight - this.width
-            this.image = document.getElementById("player")
-            this.xSpeed=0
-            this.ySpeed=0
-            this.weight=1
-        }
-        isOnGround(){
-            return this.y >= this.gameHeight - this.height
-             
+            this.width = 90
+            this.height = 120
+            this.img = document.getElementById("player")
+            this.x = 0
+            this.y = this.gameHeight - this.height
+            this.vx = 0
+            this.vy = 0
+            this.weight = 1
+            this.frameX = 0
+            this.frameY = 0
+            this.sWidth = 90
+            this.sHeight = 132
+            this.dWidth = 102
+            this.dHeight = 153
+            this.myImg = document.getElementById("player")
         }
 
-        draw(context){
-            context.fillStyle = "red"
-            context.fillRect(this.x,this.y,this.width,this.height)
-            // context.drawImage(this.image,this.x,this.y)
-            
+        
+        
+
+        isOnGround()  {
+            return this.y === this.gameHeight - this.height 
         }
-        update(input){
-            if(input.keys.includes("d")){
-                this.xSpeed = 5
-            } else if (input.keys.includes("a")){
-                this.xSpeed = -5
-            } else {
-                this.xSpeed=0
+
+        draw(context) {
+            context.fillStyle = "red"
+            context.drawImage(
+                this.myImg,
+                this.frameX * this.dWidth + BASE_SPRITE_X_OFFSET, 
+                this.frameY * this.dHeight + BASE_SPRITE_Y_OFFSET, 
+                this.sWidth,
+                this.sHeight,
+                this.x,
+                this.y,
+                this.dWidth,
+                this.dHeight)
+        }
+
+        update(input) {
+            if(input.keys.includes("d")) {
+                this.vx = 3
+                this.frameX = 0
+                this.frameY = 3
+            }  else if((input.keys.includes("a"))) {
+                this.vx = -3
+                this.frameX = 1
+                this.frameY = 2
             }
-            this.x += this.xSpeed
-           
-            if (input.keys.includes("w")){
-                this.ySpeed = -32 
-            }else{
-                this.ySpeed = 0
+               else {
+                this.vx = 0
+                this.frameX = 0
+                this.frameY = 0
             }
-            if(this.x<0) {        // Border start
+            this.x += this.vx
+             
+            if(input.keys.includes("w")) {
+                this.vy = -35    
+                this.frameX = 1
+                this.frameY = 0
+            }  else {
+                this.vy = 0
+            }
+            if(this.x<0) {
                 this.x = 0
-            }else if(this.x>this.gameWidth - this.width){
+            } else if(this.x>this.gameWidth - this.width) {
                 this.x = this.gameWidth - this.width
-            }                     // Border end
-            if(!this.isOnGround()){
-                this.ySpeed = this.ySpeed + this.weight
-                this.weight++ 
+
+            }
+            if(this.y > this.gameHeight - this.height) {
+                this.y = this.gameHeight - this.height
+            }
+         
+            
+            if(!this.isOnGround()) {
+                this.vy += this.weight
+                this.weight += 2
             } else if(this.isOnGround()) {
                 this.weight = 1
+                
             }
-            this.y += this.ySpeed
-
-        }      
-    } 
+            this.y += this.vy
+        }        
+    }
     class InputHandler {
         constructor() {
             this.keys = [] 
@@ -77,17 +112,17 @@ window.addEventListener("load",()=>{
             })
             
         }
-        // Eventlistener który doda do keys naciśnięty klawisz 
-        // Eventlistener który usunie klawisz który puścisz
-        // "onkeydown" "onkeyup" 
     }
     const input = new InputHandler()
-    const player = new Player(canvas.height,canvas.width)
-    function animate(){
+    const player = new Player(canvas.height, canvas.width)
+    function animate() {
         ctx.clearRect(0,0,canvas.width,canvas.height)
         player.draw(ctx)
         player.update(input)
+        
         requestAnimationFrame(animate)
+
     }
     animate()
-})
+    
+})  
